4961dd2233794ad453909ca8344a73c9bdcbb7b5
Valmir Sales <valmir_salesgama@hotmail.com>
2024-09-02 15:00:51 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/valmirsales/modelagemconceitual/ModelagemconceitualApplication.java
+++ src/main/java/com/valmirsales/modelagemconceitual/ModelagemconceitualApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.valmirsales.modelagemconceitual.domain.Categoria;
+import com.valmirsales.modelagemconceitual.domain.Produto;
 import com.valmirsales.modelagemconceitual.repositories.CategoriaRepository;
+import com.valmirsales.modelagemconceitual.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ModelagemconceitualApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
@@ -26,6 +31,18 @@ public class ModelagemconceitualApplication implements CommandLineRunner {
 		 Categoria cat1 = new Categoria(null, "Informática");
 		 Categoria cat2 = new Categoria(null, "Escritório");
 		 
+		 Produto p1 = new Produto(null, "Computador", 2000.00);
+		 Produto p2 = new Produto(null, "Impressora", 800.00); 
+		 Produto p3 = new Produto(null, "Mouse", 80.00); 
+		 
+		 cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		 cat2.getProdutos().addAll(Arrays.asList(p2));
+		 
+		 p1.getCategorias().addAll(Arrays.asList(cat1));
+		 p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		 p3.getCategorias().addAll(Arrays.asList(cat1));
+		 
 		 categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		 produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }

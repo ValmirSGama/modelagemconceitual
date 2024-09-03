4e66113e32195a6fb7a34315a91f93bcd374063c
Valmir Sales <valmir_salesgama@hotmail.com>
2024-09-03 16:30:52 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/valmirsales/modelagemconceitual/ModelagemconceitualApplication.java
+++ src/main/java/com/valmirsales/modelagemconceitual/ModelagemconceitualApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.valmirsales.modelagemconceitual.domain.Categoria;
 import com.valmirsales.modelagemconceitual.domain.Cidade;
+import com.valmirsales.modelagemconceitual.domain.Cliente;
+import com.valmirsales.modelagemconceitual.domain.Endereco;
 import com.valmirsales.modelagemconceitual.domain.Estado;
 import com.valmirsales.modelagemconceitual.domain.Produto;
+import com.valmirsales.modelagemconceitual.domain.enuns.TipoCliente;
 import com.valmirsales.modelagemconceitual.repositories.CategoriaRepository;
 import com.valmirsales.modelagemconceitual.repositories.CidadeRepository;
+import com.valmirsales.modelagemconceitual.repositories.ClienteRepository;
+import com.valmirsales.modelagemconceitual.repositories.EnderecoRepository;
 import com.valmirsales.modelagemconceitual.repositories.EstadoRepository;
 import com.valmirsales.modelagemconceitual.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class ModelagemconceitualApplication implements CommandLineRunner {
 	
 	@Autowired
 	public CidadeRepository cidadeRepository;
+	
+	@Autowired
+	public ClienteRepository clienteRepository;
+	
+	@Autowired
+	public EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemconceitualApplication.class, args);
@@ -67,5 +78,17 @@ public class ModelagemconceitualApplication implements CommandLineRunner {
 		 
 		 estadoRepository.saveAll(Arrays.asList(est1, est2));
 		 cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		 
+		 Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "3856376538", TipoCliente.PESSOAFISICA);
+		 
+		 cli1.getTelefones().addAll(Arrays.asList("83756365", "87456744"));
+		 
+		 Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "45673876", cli1, c1);
+		 Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "83764376", cli1, c2); 
+		 
+		 cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		 
+		 clienteRepository.saveAll(Arrays.asList(cli1));
+		 enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 }
